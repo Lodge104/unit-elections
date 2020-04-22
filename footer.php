c0d721cf41c5e1c997da9d0a38f1d85153898d95
Nicholas Anderson <nickanderson1998@gmail.com>
2020-04-22 00:13:07 -0400

    moved GA code

--- footer.php
+++ footer.php
@@ -1,14 +1,5 @@
 <footer class="container-fluid">
 <script src="https://embed.small.chat/T087CD7RPG0120P977B5.js" async></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37461006-19"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-37461006-19');
-</script>
 <div class="row">
     <div class="col-md-12">
 		<p>&copy; <?php echo date("Y"); ?> Occoneechee Lodge | Site designed and managed by the <a href="https://github.com/Lodge104/unit-elections">Lodge Website Committee</a> | <a href="https://lodge104.net/privacy-policy/">Privacy Policy</a> and <a href="https://lodge104.net/terms-of-service/">Terms of Service</a></p>
